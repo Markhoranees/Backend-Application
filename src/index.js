dc8c498ad615fba2b5f87dfd5053bc1a4651eee9
Your Name <youremail@example.com>
2024-08-02 02:57:50 +0500

    connected to mongo

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import connectDB from "../src/db/index.js";
+import dotenv from 'dotenv';
+
+
+dotenv.config({path:'./env'});
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+
+const app=express();
+
+
+
+(async ()=>{
+    try{
+        await  mongoose.connect('${process.env.MONGODB_URL}/${DB_NAME}')
+        app.on("error", (error)=>{
+            console.log("ERROR:",error)
+            throw err
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log('App is Listening on Port: ${process.env.PORT}')  
+        })
+    }
+    catch(error) {
+         console.error("ERror:",error)
+         throw err
+    }
+})()
+    */
